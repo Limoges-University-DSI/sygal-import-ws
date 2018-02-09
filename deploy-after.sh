d035cda972b2ee4dc564448f536a712486514d3a
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-02-09 11:16:17 +0100

    Scripts de déploiement

---
+++ deploy-after.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+################################################################################################################################
+#                                       Script post-déploiement.
+################################################################################################################################
+
+# Ce script a accès aux mêmes variables que le script "./deploy.sh" :
+#   - host              ex: "root@ldev"
+#   - www               ex: "/var/www"
+#   - versionsdir       ex: "/var/www/sodoct-versions"
+#   - name              ex: "sodoct"
+#   - ts                ex: "20170317-110750"
+#   - tmpdir            ex: "/var/www/sodoct-versions/tmp"
+#   - destdir           ex: "/var/www/sodoct-versions/sodoct-20170317-110750"
+
