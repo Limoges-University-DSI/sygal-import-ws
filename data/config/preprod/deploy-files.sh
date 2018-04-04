dd646164288262ba95b64df145b71e67bf0e86bb
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-04-04 15:17:30 +0200

    Déplacement configs locales dans le projet deploiement

--- data/config/preprod/deploy-files.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-dir=`dirname $0`
-appdir='/var/www/sygal-import-ws'
-set -x
-
-cp ${dir}/files/local.php      ${appdir}/config/autoload/local.php
-cp ${dir}/files/users.htpasswd ${appdir}/config/users.htpasswd
