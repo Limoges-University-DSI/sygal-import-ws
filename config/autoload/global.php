862b8edbbb57f50259325dead627342289d0db22
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-04-27 11:41:01 +0200

    Déplacement de lignes de config globales

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,5 +1,21 @@
 <?php
 return [
+    'zf-mvc-auth' => [
+        'authentication' => [
+            'adapters' => [
+                'basic' => [
+                    'adapter' => \ZF\MvcAuth\Authentication\HttpAdapter::class,
+                    'options' => [
+                        'accept_schemes' => [
+                            0 => 'basic',
+                        ],
+                        'realm' => 'api',
+                        //'htpasswd' => __DIR__ . '/../users.htpasswd',
+                    ],
+                ],
+            ],
+        ],
+    ],
     'zf-content-negotiation' => [
         'selectors' => [],
     ],
