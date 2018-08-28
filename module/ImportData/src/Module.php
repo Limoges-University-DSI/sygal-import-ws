29dadb369d82c57add9ed223d924d2cc8843caf2
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-08-28 16:20:23 +0200

    Déplacement nécessaire de Module.php suite à l'install du package zf-composer-autoloading (sans doute!)

---
+++ module/ImportData/src/Module.php
@@ -0,0 +1,12 @@
+<?php
+namespace ImportData;
+
+use ZF\Apigility\Provider\ApigilityProviderInterface;
+
+class Module implements ApigilityProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
