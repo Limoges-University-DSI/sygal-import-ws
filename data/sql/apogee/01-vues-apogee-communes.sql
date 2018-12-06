be2261be0fc39185f7d5baf81344487a7bbb67a4
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-12-06 16:02:28 +0100

    Reorganisation plus logique des scripts de création des vues : toutes les vues sont créées par le script *-communes.sql et les vues particulières à un établissement sont écrasées par le script de l'établissement.

--- data/sql/apogee/01-vues-apogee-communes.sql
+++ data/sql/apogee/01-vues-apogee-communes.sql
@@ -116,6 +116,38 @@ create view SYGAL_SOURCE as
   from dual
 /
 
+create view SYGAL_VARIABLE_MANU as
+  select
+    'apogee' as source_id,
+    'EMAIL_ASSISTANCE' as id,
+    'EMAIL_ASSISTANCE' as cod_vap,
+    'Adresse mail de l''assistance utilisateur' as lib_vap,
+    'assistance-sygal@domaine.fr' as par_vap,
+    to_date('2017-01-01', 'YYYY-MM-DD') as DATE_DEB_VALIDITE,
+    to_date('9999-12-31', 'YYYY-MM-DD') as DATE_FIN_VALIDITE
+  from dual
+  union all
+  select
+    'apogee' as source_id,
+    'EMAIL_BU' as id,
+    'EMAIL_BU' as cod_vap,
+    'Adresse mail de contact de la BU' as lib_vap,
+    'sygal-bu@domaine.fr' as par_vap,
+    to_date('2017-01-01', 'YYYY-MM-DD') as DATE_DEB_VALIDITE,
+    to_date('9999-12-31', 'YYYY-MM-DD') as DATE_FIN_VALIDITE
+  from dual
+  union all
+  select
+    'apogee' as source_id,
+    'EMAIL_BDD' as id,
+    'EMAIL_BDD' as cod_vap,
+    'Adresse mail de contact du bureau des doctorats' as lib_vap,
+    'doctorat-sygal@domaine.fr' as par_vap,
+    to_date('2017-01-01', 'YYYY-MM-DD') as DATE_DEB_VALIDITE,
+    to_date('9999-12-31', 'YYYY-MM-DD') as DATE_FIN_VALIDITE
+  from dual
+/
+
 create view SYGAL_VARIABLE as
   select
     'apogee'            as source_id,       -- Id de la source
