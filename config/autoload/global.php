a5c357bfbc8e1a1cd479a7d6704cde3ed1519048
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2019-02-01 15:35:13 +0100

    Nouvelle config pour le numéro de version + script 'bump-version'.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,7 +1,6 @@
 <?php
-return [
-    'version' => '1.2.2',
 
+return [
     'zf-mvc-auth' => [
         'authentication' => [
             'adapters' => [
---
+++ config/autoload/version.global.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'unicaen-app' => [
+        'app_infos' => [
+            'version' => '1.2.3',
+            'date' => '01/02/2019',
+        ],
+    ],
+    'comment' => 'Fichier généré le 01/02/2019 à 11:02:57 avec /home/gauthierb/workspace/sygal/bump-version',
+];
